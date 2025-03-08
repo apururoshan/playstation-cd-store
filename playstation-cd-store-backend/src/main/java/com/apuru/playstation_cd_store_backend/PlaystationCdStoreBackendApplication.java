1ca165050cb80a00ae61eb36f2cec2c689232e6c
apururoshan <apururoshan1137@gmail.com>
2025-03-08 18:56:14 +0530

    Adding frontend and backend folders

---
+++ playstation-cd-store-backend/src/main/java/com/apuru/playstation_cd_store_backend/PlaystationCdStoreBackendApplication.java
@@ -0,0 +1,13 @@
+package com.apuru.playstation_cd_store_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaystationCdStoreBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaystationCdStoreBackendApplication.class, args);
+	}
+
+}
---
+++ playstation-cd-store-backend/src/test/java/com/apuru/playstation_cd_store_backend/PlaystationCdStoreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apuru.playstation_cd_store_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaystationCdStoreBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
